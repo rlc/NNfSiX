a054334470d6eba5447c02f61eaae7dd1352a780
rlc <rlc@users.noreply.github.com>
2022-02-06 17:25:08 +0000

    Replace the code with pure Lua implementation
    The original code has variable type explicitly added to the argument,
    which cannot be interpreted by Lua interpreter out of the box.
    
    In my opinion, the example code to accompany this chapter doesn't need a
    fully-fledged matrix class implementation, therefore simplifying the
    code to be read to understand the operation.
    
    Some of the native python functions that are not available in Lua are
    also simulated with pure Lua to make the final code similar to the
    original python version.
    
    In the same fashion, the variable names are kept consistent with the
    original code.

--- Lua/p003-Dot-Product.lua
+++ Lua/p003-Dot-Product.lua
@@ -1,158 +1,91 @@
-local Matrix = {}
-Matrix.__index = Matrix
+--[[
+Doing dot product with a layer of neurons and multiple inputs
+Associated YT NNFS tutorial: https://www.youtube.com/watch?v=tMrbN67U9d4
+--]]
+--- Helper functions ----------
+-- NOTE: these functions are written to handle the use case specified in the example
+-- and hence contains only bare-minimum error handling
+local _print = print
+local function arrayToString(a)
+    local out = {}
+    for i, v in ipairs(a) do
+        if type(v) == "table" then
+            out[i] = arrayToString(v)
+        else
+            out[i] = v
+        end
+    end
 
--- Constructors {
-function Matrix.new(rows: number, cols: number, value: number?)
-	local fillValue = value or 0
-	local mat = table.create(rows, table.create(cols, fillValue))
-	setmetatable(mat, Matrix)
-	return mat
+    return string.format("{%s}", table.concat(out, ", "))
 end
 
-function Matrix.from1DArray(arr: {[number]: number})
-	local rows, cols = #arr, 1
-	local mat = {}
-	for i = 1, rows do
-		mat[i] = {}
-		for j = 1, cols do
-			mat[i][j] = arr[i]
-		end
-	end
-	setmetatable(mat, Matrix)
-	return mat	
+function print(t)
+    if type(t) == "table" then
+        -- assume all tables are arrays
+        _print(arrayToString(t))
+    else
+        _print(t)
+    end
 end
 
-function Matrix.from2DArray(arr: {[number]: {[number]: number}})
-	local rows, cols = #arr, #arr[1]
-	local mat = {}
-	for i = 1, rows do
-		mat[i] = {}
-		for j = 1, cols do
-			mat[i][j] = arr[i][j]
-		end
-	end
-	setmetatable(mat, Matrix)
-	return mat	
+function zip(a, b)
+    local i = 0
+    return function()
+        i = i + 1
+        return a[i], b[i]
+    end
 end
 
--- Mathematical Functions
+-- np simulation
+local np = {}
+local mt_npArray = {
+    __add = function(a, b)
+        local out = {}
+        for i, v in ipairs(a, b) do
+            out[i] = v + b[i] or 0
+        end
 
-function Matrix.dot(matA, matB)
-	local rowsB, colsA = #matB, #matA[1]
-	if colsA ~= rowsB then
-		error("Invalid matrix size")
-	end
-	local rowsA, colsB = #matA, #matB[1]
-	local res = {}
-	for i = 1, rowsA do
-		res[i] = {}
-		for j = 1, colsB do
-			local sum = 0
-			for k = 1, colsA do
-				sum = sum + matA[i][k] * matB[k][j]
-			end
-			res[i][j] = sum
-		end
-	end
-	setmetatable(res, Matrix)
-	return res
+        return out
+    end
+}
+
+function np.dot(ta, b)
+    local out = {}
+    setmetatable(out, mt_npArray)
+
+    -- use the first argument ta as the iterator
+    for _, a in ipairs(ta) do
+        local sumproduct = 0
+        for va, vb in zip(a, b) do
+            sumproduct = sumproduct + va*vb
+        end
+        table.insert(out, sumproduct)
+    end
+
+    return out
 end
 
-function Matrix.__add(matA, matB)
-	local rowsA, colsA = #matA, #matA[1]
-	if rowsA ~= #matB or colsA ~= #matB[1] then
-		error("Invalid matrix size")
-	end
-	local res = {}
-	for i = 1, rowsA do
-		res[i] = {}
-		for j = 1, colsA do
-			res[i][j] = matA[i][j] + matB[i][j]
-		end
-	end
-	setmetatable(res, Matrix)
-	return res
+--- Code starts here ----------
+inputs = {1.0, 2.0, 3.0, 2.5}
+weights = {{0.2, 0.8, -0.5, 1.0},
+           {0.5, -0.91, 0.26, -0.5},
+           {-0.26, -0.27, 0.17, 0.87}}
+
+biases = {2.0, 3.0, 0.5}
+
+-- First version
+layer_outputs = {} -- Output of current layer
+for neuron_weights, neuron_bias in zip(weights, biases) do
+    local neuron_output = 0 -- Output of given neuron
+    for n_input, weight in zip(inputs, neuron_weights) do
+        neuron_output = neuron_output + n_input*weight
+    end
+    neuron_output = neuron_output + neuron_bias
+    table.insert(layer_outputs, neuron_output)
 end
 
-function Matrix.__sub(matA, matB)
-	local rowsA, colsA = #matA, #matA[1]
-	if rowsA ~= #matB or colsA ~= #matB[1] then
-		error("Invalid matrix size")
-	end
-	local res = {}
-	for i = 1, rowsA do
-		res[i] = {}
-		for j = 1, colsA do
-			res[i][j] = matA[i][j] - matB[i][j]
-		end
-	end
-	setmetatable(res, Matrix)
-	return res
-end
+print(layer_outputs)
 
-function Matrix.__mul(matA, matB)
-	local rowsA, colsA = #matA, #matA[1]
-	if rowsA ~= #matB or colsA ~= #matB[1] then
-		error("Invalid matrix size")
-	end
-	local res = {}
-	for i = 1, rowsA do
-		res[i] = {}
-		for j = 1, colsA do
-			res[i][j] = matA[i][j] * matB[i][j]
-		end
-	end
-	setmetatable(res, Matrix)
-	return res
-end
-
-function Matrix.__div(matA, matB)
-	local rowsA, colsA = #matA, #matA[1]
-	if rowsA ~= #matB or colsA ~= #matB[1] then
-		error("Invalid matrix size")
-	end
-	local res = {}
-	for i = 1, rowsA do
-		res[i] = {}
-		for j = 1, colsA do
-			res[i][j] = matA[i][j] / matB[i][j]
-		end
-	end
-	setmetatable(res, Matrix)
-	return res
-end
-
--- Helpful functions
-function Matrix.__tostring(mat)
-	local rows, cols = #mat, #mat[1]
-	local str = "{"
-	for i = 1, rows do
-		str = str.."{"
-		for j = 1, cols do
-			str = str..mat[i][j]
-			if j < cols then
-				str = str..", "
-			end
-		end
-		str = str.."}"
-		if i < rows then
-			str = str..",\n"
-		end
-	end
-	str = str.."}"
-	return str
-end
-
--- Code from video in Lua
-local mat = require(script.Matrix)
-
-local inputs = mat.from1DArray({1, 2, 3, 2.5})
-local weights = mat.from2DArray({
-	{0.20, 0.80, -0.50, 1.00},
-	{0.50, -0.91, 0.26, -0.50},
-	{-0.26, -0.27, 0.17, 0.87}
-})
-local biases = mat.from1DArray({2, 3, 0.5})
-
-local output = mat.dot(weights, inputs) + biases
-print(tostring(output))
+-- Second version
+layer_outputs = np.dot(weights, inputs) + biases
+print(layer_outputs)
